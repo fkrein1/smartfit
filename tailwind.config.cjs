dbc96cc539d67195a0b156048e354324f8b7ca6b
fkrein1 <felipekrein@gmail.com>
2022-08-17 14:36:00 -0300

    add tailwind, fonts, assets, colors and favicon

---
+++ tailwind.config.cjs
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Gotham Book", "sans-serif"],
+        gotham_black: ["Gotham Black", "sans-serif"],
+        gotham_book: ["Gotham Book", "sans-serif"],
+        gotham_light: ["Gotham Light", "sans-serif"],
+        gotham_bold: ["Gotham Bold", "sans-serif"]
+      },
+      colors: {
+        yellow: {
+          500: '#FFB612',
+        },
+        gray: {
+          500: '#808080',
+          800: '#333333'
+        },
+        red: {
+          500: '#dc0a17',
+        },
+        green: {
+          500: '2FC022',
+        }
+      }
+    },
+  },
+  plugins: [],
+}
